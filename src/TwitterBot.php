e511fe6d1a09dce4bdc33a5b6e7c3b065a99d4a8
Eric Salerno <eric.salerno@viacom.com>
2017-08-17 11:03:50 -0400

    Add app level authentication

--- src/TwitterBot.php
+++ src/TwitterBot.php
@@ -27,11 +27,43 @@ class TwitterBot extends \SlzBot\IRC\Bot
     {
         parent::__construct();
 
-        $twitterInfo = json_decode(file_get_contents(__DIR__ . '/../config/twitter.json'));
+        $filename = __DIR__ . '/../config/twitter.json';
+        $twitterInfo = json_decode(file_get_contents($filename));
 
-        \Codebird\Codebird::setConsumerKey($twitterInfo->consumerKey, $twitterInfo->consumerSecret);
+        if (empty($twitterInfo))
+        {
+            echo "Can not setup twitter! Can't read contents of the json file!\n";
+            return;
+        }
+
         static::$twitter = \Codebird\Codebird::getInstance();
-        static::$twitter->setToken($twitterInfo->accessToken, $twitterInfo->accessTokenSecret);
+
+        static::$twitter->setConsumerKey($twitterInfo->consumerKey, $twitterInfo->consumerSecret);
+
+        if (!empty($twitterInfo->accessToken) && !empty($twitterInfo->accessTokenSecret))
+        {
+            static::$twitter->setToken($twitterInfo->accessToken, $twitterInfo->accessTokenSecret);
+        }
+        else
+        {
+            if (!empty($twitterInfo->bearerToken))
+            {
+                static::$twitter->setBearerToken($twitterInfo->bearerToken);
+            }
+            else
+            {
+                $reply = static::$twitter->oauth2_token();
+                $bearerToken = $reply->access_token;
+
+                $twitterInfo->bearerToken = $bearerToken;
+                if (!file_put_contents($filename, json_encode($twitterInfo, JSON_PRETTY_PRINT)))
+                {
+                    echo 'Can\'t save the token. Your bearer token is "' . $bearerToken . '" you should put this in your config file as bearerToken.' . " \n";
+                }
+
+                static::$twitter->setBearerToken($bearerToken);
+            }
+        }
 
         static::$twitterDebug = $twitterInfo->debug;
     }
